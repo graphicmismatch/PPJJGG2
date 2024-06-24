5743165e01640ce271a34eb49a66b191c64c0803
graphicmismatch <riptide.syncopate@gmail.com>
2024-06-24 12:48:58 +0530

    quick fix

---
+++ Assets/Scripts/AudioSwitcher.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class AudioSwitcher : MonoBehaviour
+{
+    public AudioSource asr;
+
+    public AudioClip[] clips;
+    public int curClip;
+
+    public void switchClip()
+    {
+        curClip++;
+        if (curClip >= clips.Length) { curClip = 0; }
+        int samp = asr.timeSamples;
+        asr.clip = clips[curClip];
+        asr.timeSamples = samp;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
